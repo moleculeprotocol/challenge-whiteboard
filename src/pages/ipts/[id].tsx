98d34838ad1bcb60494de776ed3584d1d196dfa0
stadolf <stadolf@gmail.com>
2023-08-13 23:36:03 +0200

    more form fields timelockedtoken abi
    Signed-off-by: stadolf <stadolf@gmail.com>

--- src/pages/ipts/[id].tsx
+++ src/pages/ipts/[id].tsx
@@ -1,6 +1,8 @@
 import {
+  Button,
   Flex,
   FormControl,
+  FormHelperText,
   FormLabel,
   Heading,
   Input,
@@ -19,21 +21,40 @@ const IPTPage = () => {
   return (
     <form>
       <VStack gap={6}>
-        <Heading>IPT #{iptId}</Heading>
-        <Flex direction="row" gap={2}>
+        <Flex direction="column">
+          <Heading>IPT #{iptId}</Heading>
+          <Text>Locked Token Contract: 0x</Text>
+        </Flex>
+        <Flex direction="column" gap={6}>
           <FormControl>
-            <FormLabel>Buy Amount</FormLabel>
+            <FormLabel>Lock Amount</FormLabel>
             <InputGroup>
               <Input placeholder="e.g. 1.5" />
               <InputRightAddon>
                 <Text fontSize="xs">Symbol</Text>
               </InputRightAddon>
             </InputGroup>
+            <FormHelperText>Your balance: ?</FormHelperText>
           </FormControl>
           <FormControl>
-            <FormLabel>Stake Amount</FormLabel>
-            <Input isDisabled value="0" />
+            <FormLabel>Beneficiary</FormLabel>
+            <InputGroup>
+              <Input placeholder="0xf00ba7" />
+              <InputRightAddon>
+                <Text fontSize="xs">their eth balance</Text>
+              </InputRightAddon>
+            </InputGroup>
+            <FormHelperText>Lock amount for this account</FormHelperText>
           </FormControl>
+          <FormControl>
+            <FormLabel>Lock until</FormLabel>
+            <InputGroup>
+              <Input placeholder="2023-08-31 15:00" />
+            </InputGroup>
+          </FormControl>
+          <Button colorScheme="purple" isDisabled>
+            Lock!
+          </Button>
         </Flex>
       </VStack>
     </form>
